ce81652bbf2d49ba1b916e4cb86120b80a340c33
Miles Williams <mileswilliams@gmail.com>
2026-02-13 19:45:26 +0100

    fix(deps): Switch from react-quill to react-quill-new for React 19 compatibility

--- src/components/AdminCMS.tsx
+++ src/components/AdminCMS.tsx
@@ -3,8 +3,8 @@
 import { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
 
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
-import 'react-quill/dist/quill.snow.css';
+const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
+import 'react-quill-new/dist/quill.snow.css';
 
 type Section = {
     id: number;
